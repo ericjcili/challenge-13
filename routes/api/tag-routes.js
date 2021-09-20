12c518024e9496bd1195de1fc7e68629c8b2b58c
ericjcili <ericjcili@gmai.com>
2021-09-19 20:05:21 -0400

    completed project

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -84,7 +84,7 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
-  ag.destroy({
+  Tag.destroy({
     where: {
         id: req.params.id
     }
